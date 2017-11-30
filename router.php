1502620570c1f4c068204a135742c11e6e67971c
Ivan Lim <aimprove@gmail.com>
2017-11-30 16:36:31 -0500

    Add router file to support auto add of php extension

---
+++ router.php
@@ -0,0 +1,40 @@
+<?php
+// Ref: https://stackoverflow.com/questions/27381520/php-built-in-server-and-htaccess-mod-rewrites
+// Ref: http://php.net/manual/en/features.commandline.webserver.php
+
+chdir(__DIR__);
+// Note: Somehow php knows how to deal with a mixture of / and \ in file path
+$filePath = $_SERVER['DOCUMENT_ROOT'].$_SERVER["REQUEST_URI"];
+$finalFilePath = false; // Will contain proper file path if the request is valid
+if (preg_match('/(.)+\/\.(.)+/', $filePath)) {
+    // File or directory starting with . is prohibited
+    // Note: \/ = /
+    // Note: \. = .
+    echo "Invalid request";
+} else if (is_file($filePath)) {
+    // File
+    $finalFilePath = $filePath;
+} else if (is_dir($filePath)){
+    // Directory
+    // Attempt to find an index file
+    foreach (['index.php', 'index.html'] as $indexFile){
+        $checkFile = realpath($filePath . DIRECTORY_SEPARATOR . $indexFile);
+        if (is_file($checkFile)) {
+            $finalFilePath = $checkFile;
+            break;
+        }
+    }
+} else {
+    // Auto add .php
+    $checkFile = realpath($filePath . '.php');
+    if (is_file($checkFile)) {
+        // Matches a file after add .php
+        $finalFilePath = $checkFile;
+    } else {
+        echo 'File not found: ' . $filePath;
+    }
+}
+
+if ($finalFilePath) {
+    include($finalFilePath);
+}
